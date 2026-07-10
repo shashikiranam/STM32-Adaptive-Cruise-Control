06165fa46beef9b60586fbc3cf2172a3051ce32b
SHASHI KIRAN A M <85984595+shashikiranam@users.noreply.github.com>
2026-07-10 18:19:14 +0530

    Delete firmware directory

--- firmware/Core/Inc/acc.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ACC_H
-#define __ACC_H
-
-void ACC_Init(void);
-
-void ACC_Update(void);
-
-#endif
--- firmware/Core/Inc/gpio.h
+++
@@ -1,6 +0,0 @@
-#ifndef __GPIO_H
-#define __GPIO_H
-
-void GPIO_Init_Custom(void);
-
-#endif
