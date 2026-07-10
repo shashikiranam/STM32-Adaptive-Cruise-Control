3e2c854349c7b72155d03f293a08904cccfb7ac3
SHASHI KIRAN A M <85984595+shashikiranam@users.noreply.github.com>
2026-07-10 10:09:16 +0530

    Add header guards and include necessary headers

--- firmware/Core/Inc/main.h
+++ firmware/Core/Inc/main.h
@@ -1,1 +1,17 @@
+#ifndef __MAIN_H
+#define __MAIN_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "ultrasonic.h"
+#include "motor.h"
+#include "pwm.h"
+#include "acc.h"
+#include "gpio.h"
+#include "timer.h"
+#include "system_config.h"
+
+void System_Init(void);
+
+#endif
